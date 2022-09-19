c59bc8a0a8edc7e000e3bd2eff0a27a3699a5d38
Ian <arcanecenotaph@gmail.com>
2022-09-18 22:55:33 -0500

    first commit

---
+++ cc_for_loop1.py
@@ -0,0 +1,7 @@
+""" 
+Change the value in the print function to print out the iteration variable i so we can see it decrement.
+Change the start from 1 to 20, end from 10 to -21 and increment from 1 to -5.
+ """
+
+for i in range(20, -21, -5):
+    print(i)
